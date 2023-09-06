90ecbb4e0e6de4865a09927fd92c739d9ca105c4
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-09-06 17:19:50 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Бабинова Софья Евгеньевна, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+        
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Бабинова Софья, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""

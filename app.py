71f337a8d98764461ecab808c2e7680f7b411f99
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-09-06 17:51:01 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return redirect("/menu", code=302)
+
+@app.route("/menu")
+def menu():
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+        
+        <ol>
+            <li>
+                <a href="/lab1" target="_blank">Лабораторная работа 1</a>
+            </li>
+        </ol>
+
+        <footer>
+            &copy; Бабинова Софья, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!DOCTYPE html>
 <html>
@@ -14,7 +45,13 @@ def start():
             НГТУ, ФБ, Лабораторная работа 1
         </header>
         
-        <h1>web-сервер на flask</h1>
+        <h1>
+        Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов
+        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </h1>
 
         <footer>
             &copy; Бабинова Софья, ФБИ-14, 3 курс, 2023

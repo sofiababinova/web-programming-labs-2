322172165ff6c19001fa7f5ea15e77d68d2b9af8
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-09-20 17:28:45 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -190,3 +190,7 @@ def nstu():
     </body>
 </html>
 '''
+
+@app.route('/lab2/example')
+def example():
+    return render_template ('example.html')

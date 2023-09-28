837fb8e5b6fc0d4c4ee625e86a20604bf27b30fd
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-09-28 20:29:36 +0700

    Книги

--- app.py
+++ app.py
@@ -201,4 +201,17 @@ def example():
         {'name': 'мандарины', 'price': 95},
         {'name': 'манго', 'price': 321}
     ]
-    return render_template ('example.html' , name=name, group=group, course=course, lab_num=lab_num, fruits=fruits)
+
+    books = [
+        {'book': 'Мастер и Маргарита', 'author': 'Михаил Булгаков', 'genre': 'Роман', 'pages': 528},
+        {'book': 'По ком звонит колокол', 'author': 'Эрнест Хэмингуэй', 'genre': 'Роман', 'pages': 704},
+        {'book': 1984, 'author': 'Джордж Оруэлл', 'genre': 'Роман', 'pages': 320},
+        {'book': 'Над пропастью во ржи', 'author': 'Джером Дэвид Сэлинджер', 'genre': 'Роман', 'pages': 234},
+        {'book': 'Маленький принц', 'author': 'Антуан де Сент-Экзюпери', 'genre': 'Сказка', 'pages': 128},
+        {'book': 'Гамлет', 'author': 'Уильям Шекспир', 'genre': 'Трагедия', 'pages': 384},
+        {'book': 'Три товарища', 'author': 'Эрих Мария Ремарк', 'genre': 'Роман', 'pages': 480},
+        {'book': 'Великий Гэтсби', 'author': 'Фрэнсис Скотт Фицджеральд', 'genre': 'Роман', 'pages': 256},
+        {'book': '451° по Фаренгейту', 'author': 'Рэй Бредбери', 'genre': 'Роман', 'pages': 256},
+        {'book': 'Убить пересмешника', 'author': 'Харпер Ли', 'genre': 'Роман', 'pages': 416}
+    ]
+    return render_template ('example.html', name=name, group=group, course=course, lab_num=lab_num, fruits=fruits, books=books)

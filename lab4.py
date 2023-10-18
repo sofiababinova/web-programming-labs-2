637bd45952e06383e6ac037887b332629cdc6784
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-10-18 16:31:06 +0700

    Куки - цвет текста

--- lab4.py
+++ lab4.py
@@ -103,3 +103,15 @@ def seed():
         skidka = ""
     
     return render_template('seed.html', weight=weight, seed=seed, price=price, errors=errors, skidka=skidka)
+
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET':
+        return render_template('cookies.html')
+    
+    color = request.form.get('color')
+    headers = {
+        'Set-Cookie': 'color=' + color + '; path=/',
+        'Location': '/lab4/cookies'
+    }
+    return '', 303, headers

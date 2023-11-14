b8cb74432e714f6558952ad9015a1ca95ed2d971
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-11-14 13:08:15 +0700

    Вывод пользователей

--- lab1.py
+++ lab1.py
@@ -37,9 +37,6 @@ def menu():
             <li>
                 <a href="/lab4/" target="_blank">Лабораторная работа 4</a>
             </li>
-            <li>
-                <a href="/lab5/" target="_blank">Лабораторная работа 5</a>
-            </li>
         </ol>
         <footer>
             &copy; Бабинова Софья, ФБИ-14, 3 курс, 2023
--- lab5.py
+++ lab5.py
@@ -1,7 +1,56 @@
-from flask import Blueprint, render_template, request
+from flask import redirect, Blueprint, render_template, request
+import psycopg2
+
+
 lab5 = Blueprint('lab5', __name__)
+
+def dbConnect():
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database="knowledge_base_for_sofia",
+        user="sofia_knowledge_base",
+        password="111")
+    
+    return conn;
+
+
+def dbClose(cursor, connection):
+    cursor.close()
+    connection.close()
 
 
 @lab5.route('/lab5/')
-def lab():
-    return render_template('lab5.html')
+def main():
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users;")
+
+    result = cur.fetchall()
+   
+    print(result)
+
+
+    dbClose(cur, conn)
+
+    return 'go to console'
+
+
+@lab5.route('/lab5/users')
+def users():
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users;")
+    
+    result = cur.fetchall()
+
+    users = []
+
+    for i in range(len(result)):
+        users.append(result[i][1])
+
+
+    dbClose(cur, conn)
+
+    return render_template('users.html', users=users)

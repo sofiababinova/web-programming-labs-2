26b4f6a9aba36be09f65a9b3fbfe1af4e9b54f50
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-11-29 16:51:42 +0700

    Адаптация лабораторной работы No5 к системе ORM

--- Db/models.py
+++ Db/models.py
@@ -2,10 +2,13 @@ from . import db
 from flask_login import UserMixin
 
 
-class users(db.Model):
+class users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(102), nullable=False)
+
+    def __repr__(self):
+        return f'id:{self.id}, username:{self.username}'
 
 class articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +18,6 @@ class articles(db.Model):
     is_favorite = db.Column(db.Boolean)
     is_public = db.Column(db.Boolean)
     likes = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'title:{self.title}, article_text:{self.article_text}'
--- lab6.py
+++ lab6.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
-lab6 = Blueprint('lab6', __name__)
+from flask import Blueprint, request, render_template, redirect 
+from Db import db
+from Db.models import users, articles 
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
+
+lab6 = Blueprint ("lab6", __name__)
 
 
-@lab6.route('/lab4/')
-def lab():
-    return render_template('lab6.html')
+@lab6. route("/lab6/check")
+def main():
+    my_users = users.query.all()
+    print (my_users)
+    return "result in console!"

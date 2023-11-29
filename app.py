c90563b34ebfd967c25651ea91c090f89c61a0ec
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-11-29 15:58:58 +0700

    Подключение ORM к проекту

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy 
+from lab6 import lab6 
+from Db import db 
+from Db.models import users 
+from flask_login import LoginManager
 from flask import Flask
+
+
+app = Flask(__name__)
+
+
+app.secret_key = "123"
+user_db = "admin_knowledge_base_orm"
+host_ip = "127.0.0.1"
+host_port = "5432"
+database_name = "knowledge_base_orm"
+password = "123"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -7,8 +30,6 @@ from lab5 import lab5
 from lab6 import lab6
 
 
-app = Flask(__name__)
-app.secret_key = "123"
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)

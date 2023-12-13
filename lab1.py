4a40e039d2173589d9902af0d8c2505625b1772b
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-12-13 17:05:24 +0700

    Заготовка к лабораторной работе 7

--- lab1.py
+++ lab1.py
@@ -43,6 +43,9 @@ def menu():
             <li>
                 <a href="/lab6/" target="_blank">Лабораторная работа 6</a>
             </li>
+            <li>
+                <a href="/lab7/" target="_blank">Лабораторная работа 7</a>
+            </li>
         </ol>
         <footer>
             &copy; Бабинова Софья, ФБИ-14, 3 курс, 2023

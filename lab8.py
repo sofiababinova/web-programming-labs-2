dc20f47adc93c679a5f82bbed352587ec8cb9f52
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-12-20 01:02:20 +0700

    Получение информации о курсе/-ах по REST

--- lab8.py
+++ lab8.py
@@ -6,3 +6,20 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
+
+courses = [
+    {"name": "C++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "C#", "videos": 8}
+]
+
+
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return courses
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    return courses[course_num]

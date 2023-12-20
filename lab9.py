7c200c5b97cfbb24f43373d0f76e642d5ab0f9a8
Софья Бабинова <sofia.babinova.83@gmail.com>
2023-12-20 12:43:45 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -6,3 +6,10 @@ lab9 = Blueprint('lab9', __name__)
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return render_template('lab9/error404.html'), 404
+
+
